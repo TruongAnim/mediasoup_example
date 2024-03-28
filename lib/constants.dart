25439092e29c8f7143617ff215abc070d4c17695
TruongAnim <truonganim.dev@gmail.com>
2024-03-28 17:05:20 +0700

    init project

---
+++ lib/constants.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static final theme = ThemeData(
+    backgroundColor: Color(0xfffcfcff),
+    primaryColor: Color(0xfffcfcff),
+    scaffoldBackgroundColor: Color(0xfffcfcff),
+    appBarTheme: AppBarTheme(
+      elevation: 0,
+    ),
+    textSelectionTheme: TextSelectionThemeData(
+      cursorColor: Colors.orangeAccent,
+    ),
+  );
+}
